af552655016ce486de4cbf5edf1f6ac2c8ef8aaf
Angel Martinez <angel.codes139@gmail.com>
2020-09-29 17:19:44 -0500

    :sparkles: feature(tasks): remove task

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const TasksContainer = ({ state, setState }) => {
+  // Move task to another column
   const handleDragEnd = data => {
-    console.log(data);
     const { destination, source } = data;
-    console.log('from', source);
-    console.log('to', destination);
 
-    // Prevent to drop in no box
+    // Prevent to drop in no column
     if (!destination) return;
 
-    // Prevent to drop in same box
+    // Prevent to drop in same column
     if (
       destination.index === source.index &&
       destination.droppableId === source.droppableId
@@ -38,6 +36,17 @@ const TasksContainer = ({ state, setState }) => {
         taskCopy
       );
       return object;
+    });
+  };
+
+  // Delete a task
+  const handleRemove = (id, column) => {
+    setState({
+      ...state,
+      [column]: {
+        ...state[column],
+        items: [...state[column].items.filter(task => task.id !== id)]
+      }
     });
   };
 
@@ -70,9 +79,27 @@ const TasksContainer = ({ state, setState }) => {
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
-                          className="mb-2 px-4 py-3 text-center bg-white rounded-md shadow"
+                          className="mb-2 px-4 py-3 bg-white rounded-md shadow flex justify-between items-center"
                         >
                           <p className="text-gray-600">{task.description}</p>
+                          <button
+                            onClick={() => handleRemove(task.id, columnName)}
+                          >
+                            <svg
+                              className="w-6 h-6 text-red-500"
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                              ></path>
+                            </svg>
+                          </button>
                         </li>
                       )}
                     </Draggable>
